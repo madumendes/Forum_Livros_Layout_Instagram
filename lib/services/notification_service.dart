f9534039f1bd86b260ce028762829a4e0d991dfc
Maria Eduarda Mendes <149280318+madumendes@users.noreply.github.com>
2025-11-27 08:08:12 -0300

    Update notification_service.dart

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 class NotificationService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-  // 1. INICIALIZAR O SERVIÇO
+
   Future<void> initNotifications() async {
     // Pede permissão ao usuário (Crítico para Android 13+ e iOS)
     NotificationSettings settings = await _firebaseMessaging.requestPermission(
@@ -18,21 +18,17 @@ class NotificationService {
         print('Permissão de notificação concedida!');
       }
 
-      // 2. OBTER O TOKEN (Identidade do celular)
-      // Você usaria esse token para enviar msg específica para este usuário
       final fcmToken = await _firebaseMessaging.getToken();
       if (kDebugMode) {
         print('Token do Dispositivo: $fcmToken');
       }
 
-      // 3. OUVIR NOTIFICAÇÕES COM O APP ABERTO (Foreground)
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
         if (kDebugMode) {
           print('Recebi uma notificação enquanto o app estava aberto:');
           print('Título: ${message.notification?.title}');
           print('Corpo: ${message.notification?.body}');
         }
-        // Aqui você poderia mostrar um "SnackBar" ou um diálogo
       });
     } else {
       if (kDebugMode) {
